01412da4abe42b139658db548ab36ab403766366
ATULYADAVpro <atul088793151@gmail.com>
2024-06-11 22:57:57 -0700

    complete update user functionalty

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -1,19 +1,22 @@
 import { useEffect, useRef, useState } from "react"
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage"
 import { app } from "../firebase"
+import { updateUserFailure, updateUserSuccess, updateUserStart } from "../redux/user/userSlice"
 
 export default function Profile() {
-  const { currentUser } = useSelector(state => state.user)
+  const { currentUser, loading, error } = useSelector(state => state.user)
   const fileRef = useRef(null);
   const [file, setFile] = useState(undefined);
   const [filePercentage, setFilePercentage] = useState(0);
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({})
+  const dispatch = useDispatch();
+  const [updateSuccess, setUpdateSuccess] = useState(false)
 
-  console.log(filePercentage)
-  console.log(fileUploadError)
-  console.log(formData)
+  // console.log(filePercentage)
+  // console.log(fileUploadError)
+  console.log(currentUser)
 
   useEffect(() => {
     if (file) {
@@ -45,10 +48,38 @@ export default function Profile() {
 
   }
 
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(updateUserFailure(data.message));
+        return;
+      }
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true)
+    } catch (error) {
+      dispatch(updateUserFailure(error.message))
+    }
+
+  }
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input onChange={(e) => setFile(e.target.files[0])} type="file" ref={fileRef} hidden accept="image/*" />
         <img onClick={() => fileRef.current.click()} src={formData.avatar || currentUser.avatar} alt="profile-img" className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2" />
 
@@ -56,16 +87,18 @@ export default function Profile() {
           {fileUploadError ? (<span className="text-red-700">Error Image Upload (img must be less then 2 mb and only img formData) </span>) : (filePercentage > 0 && filePercentage < 100) ? (<span className="text-slate-700">{`Uploading ${filePercentage}%`}</span>) : (filePercentage === 100) ? (<span className="text-green-700">Successfully uploaded!</span>) : ""}
         </p>
 
-        <input type="text" id="username" placeholder="username" className="border p-3 rounded-lg" />
-        <input type="text" id="email" placeholder="email" className="border p-3 rounded-lg" />
-        <input type="text" id="password" placeholder="password" className="border p-3 rounded-lg" />
+        <input type="text" onChange={handleChange} defaultValue={currentUser.username} id="username" placeholder="username" className="border p-3 rounded-lg" />
+        <input type="text" onChange={handleChange} defaultValue={currentUser.email} id="email" placeholder="email" className="border p-3 rounded-lg" />
+        <input type="text" onChange={handleChange} defaultValue={currentUser.password} id="password" placeholder="password" className="border p-3 rounded-lg" />
 
-        <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">update</button>
+        <button disabled={loading} className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">{loading ? "Loading..." : "Upadte"}</button>
       </form>
       <div className="flex justify-between mt-5">
         <span className="text-red-700 cursor-pointer ">Delete account</span>
         <span className="text-red-700 cursor-pointer ">Sign Out</span>
       </div>
+      <p className="text-red-700 mt-5">{error ? error : ""}</p>
+      <p className="text-green-700 mt-5">{updateSuccess ? "Update successfull! " : ""}</p>
     </div>
   )
 }
--- server/utils/JwtService.js
+++ server/utils/JwtService.js
@@ -2,7 +2,7 @@ import { JWT_SECRECT } from "../configs/index.js";
 import jwt from "jsonwebtoken"
 class JwtService {
     static sign(payload, expiry = '60m', secret = JWT_SECRECT) {
-        return jwt.sign(payload, secret, { expiresIn: expiry })
+        return jwt.sign(payload, secret)
     }
 
     static verify(token, secret = JWT_SECRECT) {
