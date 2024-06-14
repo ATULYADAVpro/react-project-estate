f3fe32ef1bcef09be05757ecbc4e98685ef0f21f
ATULYADAVpro <atul088793151@gmail.com>
2024-06-14 02:36:34 -0700

    complete listing & context

--- server/routers/user.router.js
+++ server/routers/user.router.js
@@ -9,5 +9,6 @@ userRoutes.get("/test",userController.test)
 userRoutes.post("/update/:id",verifyToken,userController.updateUser)
 userRoutes.delete("/delete/:id",verifyToken,userController.deleteUser)
 userRoutes.get("/listings/:id",verifyToken,userController.getUserListing)
+userRoutes.get("/:id",verifyToken,userController.getUser)
 
 export default userRoutes;
