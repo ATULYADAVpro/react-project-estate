d35e2b1cd3593525e852f3572ae2308f821ef6e8
ATULYADAVpro <atul088793151@gmail.com>
2024-06-14 23:56:50 -0700

    complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import PrivateRoute from './components/PrivateRoute'
 import CreateListing from './Pages/CreateListing'
 import UpdateListing from './Pages/UpdateListing'
 import Listing from './Pages/Listing'
+import Search from './Pages/Search'
 
 function App() {
 
@@ -27,6 +28,7 @@ function App() {
           <Route path="/update-listing/:listingId" element={<UpdateListing />} />
         </Route>
         <Route path="/listing/:listingId" element={<Listing />} />
+        <Route path="/search" element={<Search />} />
       </Routes>
     </BrowserRouter>
   )
